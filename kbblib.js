d52637e8a0e3aacd795e0f8ceba409550151b756
KeBaob <ke_lihong@163.com>
2019-06-24 14:19:43 +0800

    修改input限定只输入数字bug , 只要引入并添加class = '

--- kbblib.js
+++ kbblib.js
@@ -1,12 +1,15 @@
 // 限制文本框：只输入数字：
+$(function () {
+    function kbbCheckNum(obj) {
+        function changeNum() {
+            var fval = $(this).val();
+            var reg = /[^0-9]+/;
+            $(this).val(fval.replace(reg, ''));
+        }
 
-function kbbCheckNum(obj) {
-    function changeNum() {
-        var fval = $(this).val();
-        var reg = /[^0-9]+/;
-        $(this).val(fval.replace(reg, ''));
+        $(obj).bind("input propertychange", changeNum);
     }
+    // kbbCheckNum(".kbb-input-num");
+    kbbCheckNum(".kbb-input-num");
 
-    $(obj).bind("input propertychange", changeNum);
-}
-
+})
